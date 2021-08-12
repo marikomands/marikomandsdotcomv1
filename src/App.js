715decb66f147061f0d736d863b39534f72b4c5f
marikomands <marikomands@gmail.com>
2021-08-12 11:09:08 +0100

    Website Init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+//  Content Ideas
+// Focus on coding skills you've learned
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Mariko Mands</p>
         <a
           className="App-link"
           href="https://reactjs.org"
